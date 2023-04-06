396136f89829e9e30c0931e78d52bb07c0b117fa
TGStarGuardian <80158819+TGStarGuardian@users.noreply.github.com>
2023-04-06 21:43:02 +0200

    Delete fol.cpp

--- fol.cpp
+++
@@ -1,5 +0,0 @@
-#include "fol.hpp"
-
-shared_ptr<TermDatabase> TermDatabase::_term_database = nullptr;
-shared_ptr<FormulaDatabase> FormulaDatabase::_formula_database = nullptr;
-Formula parsed_formula = nullptr;
